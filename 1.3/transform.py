a2be22d65bef90568d4b34d650d1a112ae83aff7
Edward Banner <edward@Edwards-MacBook-Air.local>
2025-12-23 16:56:21 -0500

    Add rot90 and rot180

---
+++ 1.3/transform.py
@@ -0,0 +1,49 @@
+def copy(A):
+    m = len(A[0])
+    return [[0]*m for row in A]
+
+def rot90(A):
+    A_rot = copy(A)
+
+    n, m = len(A), len(A[0])
+
+    J = range(m-1, -1, -1)
+    I = range(n)
+
+    for k in range(n):
+        for j, i in zip(J, I):
+            A_rot[i][k] = A[k][j]
+
+    return A_rot
+
+
+def rot180(A):
+    A_rot = rot90(A)
+    A_rot_rot = rot90(A_rot)
+    return A_rot_rot
+
+
+def rot270(A):
+    A_rot = rot90(A)
+    A_rot_rot = rot90(A_rot)
+    return A_rot_rot
+
+
+if __name__ == '__main__':
+    A = [['@', '@'], 
+        ['-', '-']]
+
+    for row in A:
+        print(row)
+    print()
+
+    A_rot = rot90(A)
+
+    for row in A_rot:
+        print(row)
+    print()
+
+    A_rot_rot = rot90(A_rot)
+
+    for row in A_rot_rot:
+        print(row)
